ed90492792ca63bc975010d75392874d455fc575
Stilyan Atanasov <stilyan2008@gmail.com>
2024-10-11 21:47:37 +0300

    Updated Section 12. Object-Oriented Programming (OOP) with JavaScript
    - Added coding challenge 1

---
+++ 12. Object-Oriented Programming (OOP) with JavaScript/Coding Challenges/Coding challenge 1/Coding challenge 1.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// ----- Coding Challenge 1 -----
+
+// 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+// 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+// DATA CAR 1: 'BMW' going at 120 km/h
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+
+// --- 1 ---
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+// --- 2 ---
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+// --- 3 ---
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+// --- 4 ---
+const bmw = new Car(`BMW`, 120);
+const mercedes = new Car(`Mercedes`, 95);
+
+bmw.accelerate();
+bmw.brake();
+bmw.brake();
+
+mercedes.brake();
+mercedes.accelerate();
